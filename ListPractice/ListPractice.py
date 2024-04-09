fa5e177f17e9662456587c46345d963fc08d340b
pariisaakhanjani <p.khanjani96@gamil.com>
2024-04-09 12:59:33 +0200

    Add project files.

---
+++ ListPractice/ListPractice.py
@@ -0,0 +1,10 @@
+numbers = []
+costumer_input = input ("please enter numbers separated by comma, so that I sum them up for you:")
+costumer_input_splitted = costumer_input.split(",")
+for item in costumer_input_splitted:
+    itemchanged = int(item)
+    numbers.append(itemchanged)
+print(numbers)
+
+
+print(sum(numbers))
